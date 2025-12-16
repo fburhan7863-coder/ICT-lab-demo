ddf88e28285a693a1839b2224b17016da67e949e
ayeshaaa0134 <241419@students.au.edu.pk>
2025-12-16 20:56:17 +0500

    oop file

---
+++ oop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Test {
+public:
+    void show() {
+        cout << "Hello OOP";
+    }
+};
+
+int main() {
+    Test t;
+    t.show();
+    return 0;
+}
